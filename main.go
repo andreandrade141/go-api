286e24cc117190e86e0ceee1fde9b61c10bf6a2f
andreandrade141 <andrelsandrade43@gmail.com>
2024-12-22 00:15:48 -0300

    feature - Add missing data for generic database connection

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"go-api/controllers"
+	"go-api/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	models.ConnectDB()
+
 	r := gin.Default()
 
 	public := r.Group("/api")
--- models/setup.go
+++ models/setup.go
@@ -1,21 +1,13 @@
 package models
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 )
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"os"
-
-// 	"github.com/jinzhu/gorm"
-// 	"github.com/jinzhu/gorm/dialects/mysql"
-// 	"github.com/joho/godotenv"
-// )
 
 var DB *gorm.DB
 
@@ -25,5 +17,25 @@ func ConnectDB() {
 	if err != nil {
 		log.Fatalf("Error Loading ENV file")
 	}
+
+	Dbdriver := os.Getenv("DB_DRIVER")
+	DbHost := os.Getenv("DB_HOST")
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+	DbPort := os.Getenv("DB_PORT")
+
+	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
+
+	DB, err = gorm.Open(Dbdriver, DBURL)
+
+	if err != nil {
+		fmt.Println("Cannot connect to database ", Dbdriver)
+		log.Fatal("connection error:", err)
+	} else {
+		fmt.Println("We are connected to the database ", Dbdriver)
+	}
+
+	DB.AutoMigrate(&User{})
 
 }
